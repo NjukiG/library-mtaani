b3cf5131c159fa81a3a99b239215e541103eb358
NjukiG <gnjuki19@gmail.com>
2024-11-28 12:59:22 +0300

    Trying to connect fronend and backend after deploys.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Library Mtaani
 
 Backend for a library management system to be used for book purchase and borrows.
+
+
+### LIVE API: https://library-mtaani.onrender.com
+
+NB:In order for the app to work when deploying, comment out the loadEnv function in main.
--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 	// Apply CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Adjust this as needed
+		AllowOrigins:     []string{"*"}, // Adjust this as needed
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
